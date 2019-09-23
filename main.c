11d047d98ea4e0b57403c9165e409a019187a221
Matthew Jiang <myj99@utexas.edu>
2019-09-22 22:38:24 -0500

    added main controls, added drawing face #s

--- main.c
+++ main.c
@@ -7,10 +7,35 @@
 #include "../inc/st7735.h"
 
 int time_ofst = 0;
-int hour;
-int minute;
-int second;
-int ms;
+int showAlarm;
+int is24Hr;
+
+int mode;
+int refresh;
+int delay;
+
+int setPhase;     //phase of time adjustment
+
+int alarmH;       //alarm time
+int alarmM;
+int alarmPhase;   //phase of alarm adjustment
+
+int sw_start;     //stopwatch start time
+int sw_show;      //stopwatch show time
+int sw_on;
+
+int tm_start;     //timer start time
+int tm_duration;  //timer duration
+int tm_set;       //is currently setting timer
+int tm_h;         //variables used in setting the timer
+int tm_m;
+int tm_on;
+
+
+
+int button1;
+int button2;
+int button3;
 
 extern uint32_t time;
 
@@ -20,22 +45,122 @@ int main(){
 	Speaker_Init();
 	Timer_Init();
   Face_Init();
-  int flip = 1;
+  mode = 0;
+  delay = -1;
+  refresh = 1;
+  showAlarm = 0;
+  is24Hr = 0;
 	while(1){
-		//if(Clock Mode)
-    int ttime = time_ofst + time;
-    ms = ttime%1000;
-    second = (ttime/1000)%60;
-    minute = (ttime/60000)%60;
-    hour = (ttime/3600000)%24;
-    Face_SetTime(hour,minute,second,ms);
-    Face_Out();
-		//if(Stopwatch Mode)		
-		flip = 1 - flip;
-    if (flip) ST7735_DrawPixel(1,1,ST7735_BLACK);
-    else ST7735_DrawPixel(1,1,ST7735_BLUE);
+    if (button3){
+      mode = (mode + 1)%5;
+      delay = 100000;
+      refresh = 1;
+    }
+    if (delay == 0){
+      mode = 0;
+      refresh = 1;
+    } else delay--;
+    //return to main clock if uninteractive
+    int displayTime;
+    switch(mode){
+      case 0: // main clock
+        if (refresh){
+          Face_Set24(is24Hr);
+          Face_ShowAlarm(showAlarm);
+          Face_SetLabel("Clock");
+          refresh = 0;
+        }
+        displayTime = time_ofst + time;
+        Face_SetTimeMS(displayTime);
+        Face_Out(0);
+        break;
+      case 1: // set time
+        if (refresh){
+          setPhase = 0;
+          refresh = 0;
+          Face_SetLabel("Set Hour");
+        }
+        switch(setPhase){
+          case 0: //set hour
+            if (button1) time_ofst += 3600000;
+            if (button2){
+              setPhase++;
+              Face_SetLabel("Set Minute");
+            }
+            break;
+          case 1: //set min
+            if (button1) time_ofst += 60000;
+            if (button2){
+              setPhase++;
+              Face_SetLabel("Set Second to 0");
+            }
+            break;
+          case 2: //reset sec to 0
+            if (button1) time_ofst %= 60000;
+            if (button2){
+              setPhase++;
+            }
+            break;
+          case 3: //24 hr time or ampm
+            if (is24Hr){
+              Face_SetLabel("Show 24h time? (Y)");
+            } else Face_SetLabel("Show 24h time? (N)");
+            if (button1){
+              is24Hr = !is24Hr;
+              Face_Set24(is24Hr);
+            }
+            if (button2){
+              setPhase = 0;
+              Face_SetLabel("Set Hour");
+            }
+            break;
+        }
+        Face_SetTimeMS(time_ofst + time);
+        Face_Out(0);
+        break;
+      case 2: // alarm 
+        if(refresh){
+          if (showAlarm) Face_SetLabel("Alarm on");
+          else Face_SetLabel("Alarm off");
+          alarmPhase = 0;
+        }
+        switch(alarmPhase){
+          case 0:
+            if (button1){
+              showAlarm = !showAlarm;
+              if (showAlarm) Face_SetLabel("Alarm on");
+              else Face_SetLabel("Alarm off");
+            }
+            if (button2){
+              alarmPhase++;
+              Face_SetLabel("Set Alarm Hour");
+            }
+            break;
+          case 1:
+            if (button1){
+              alarmH = alarmH % 24;
+            }
+            if (button2){
+              alarmPhase++;
+              Face_SetLabel("Set Alarm Min");
+            }
+            break;
+          case 2:
+            if (button1){
+              alarmM = alarmM % 60;
+            }
+            break;
+        }
+        Face_SetTime(alarmH,alarmM,0,0);
+        Face_Out(0);
+        break;
+    }
+    
+		//if(Stopwatch Mode)
 		
 		//if(Timer Mode)
+    
+    
     
 		
 		
